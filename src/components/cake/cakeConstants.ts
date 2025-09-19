fda319188b5a06da9b97779b4db352a3866dba2a
Hexra Tech <hexratech0@gmail.com>
2025-09-19 12:25:51 +0000

    Update cakeConstants.ts

--- src/components/cake/cakeConstants.ts
+++ src/components/cake/cakeConstants.ts
@@ -1,36 +1,36 @@
 // cakeConstants.ts
 
 export const flavorPrices: { [key: string]: number } = {
-    "Vanilla": 10,
-    "Chocolate": 12,
-    "Red Velvet": 15,
-    "Strawberry": 13,
-    "Lemon": 11,
+    "Vanilla": ,
+    "Chocolate": ,
+    "Red Velvet": ,
+    "Strawberry": ,
+    "Lemon": ,
 };
 
 export const sizePrices: { [key: string]: number } = {
-    "6-inch": 20,
-    "8-inch": 30,
-    "10-inch": 45,
-    "12-inch": 60,
+    "6-inch": 615,
+    "8-inch": 410,
+    "9-inch":510,
+    "10-inch":615,
 };
 
 export const layerPrices: { [key: string]: number } = {
-    "Single": 0,
-    "Double": 10,
-    "Triple": 20,
+    "Single": ,
+    "Double": ,
+    "Triple": ,
 };
 
 export const icingPrices: { [key: string]: number } = {
-    "Buttercream": 5,
-    "Fondant": 15,
-    "Cream Cheese": 8,
+    "Buttercream":,
+    "Fondant":,
+    "Cream Cheese":,
 };
 
 export const toppingPrices: { [key: string]: number } = {
-    "Sprinkles": 2,
-    "Chocolate Chips": 3,
-    "Fresh Berries": 5,
-    "Edible Flowers": 7,
-    "Gold Flakes": 10,
+    "Sprinkles":,
+    "Chocolate Chips":,
+    "Fresh Berries": ,
+    "Edible Flowers": ,
+    "Gold Flakes": ,
 };
