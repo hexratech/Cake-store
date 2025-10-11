e2c8ad8c1214ace513c1e795946a622fe5423670
O6SPIDER <arnoldagyemang14@gmail.com>
2025-10-10 20:21:11 -0500

    cloudflare

--- src/admin/pages/Custom Cakes.tsx
+++ src/admin/pages/Custom Cakes.tsx
@@ -185,7 +185,7 @@ export default function CustomCakes() {
           </div>
         )}
 
-        {/* Cake Details Dialog */}
+        
         {selectedCake && (
           <CustomCakeDetails
             open={open}
